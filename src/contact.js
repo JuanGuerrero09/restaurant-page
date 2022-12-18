1cf61b28cae2ff95a904fba6113e1c2a98d6e584
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2022-12-17 23:34:18 -0500

    map added

--- src/contact.js
+++ src/contact.js
@@ -6,9 +6,9 @@ function createContact(){
     contactTitle.classList.add('title')
     contactTitle.innerText = 'Contáctenos'
 
-   const mapDirection = document.createElement('img')
-   mapDirection.src = './images/apprendistadirection.jpg'
-   mapDirection.classList.add('map-image')
+   const mapDirection = document.createElement('div')
+   mapDirection.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.9940000992688!2d-73.11764178522586!3d7.126689294853766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e681529d99a2f0b%3A0x944172f70d803820!2sApprendista!5e0!3m2!1ses!2sco!4v1671337612049!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+   mapDirection.classList.add('map-direction')
 
    const directionDetails = document.createElement('div')
    directionDetails.classList.add('direction-details')
@@ -18,7 +18,7 @@ function createContact(){
    Cra. 30a #31-27, Bucaramanga, Santander`
 
    const contactHours = document.createElement('p')
-   contactHours.innerHTML = '<i class="fa-solid fa-clock"></i>Horario<br> 12:00 - 16:00 & 18:00 - 22:00'
+   contactHours.innerHTML = '<i class="fa-solid fa-clock"></i>  Horario<br> 12:00 - 16:00 & 18:00 - 22:00'
 
     directionDetails.append(contactDirection, contactHours)
     
